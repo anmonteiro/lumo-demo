cb1ffe9170ecd51951e41e3dadcea87152c7939d
António Nuno Monteiro <anmonteiro@gmail.com>
2017-03-26 14:34:47 -0700

    init

---
+++ src/demo/core.cljs
@@ -0,0 +1,9 @@
+(ns demo.core
+  (:require [cljs.nodejs :as node]))
+
+(node/enable-util-print!)
+
+(def left-pad (js/require "left-pad"))
+
+;; pad the number 42 with five zeros
+(println "padded 42:" (left-pad 42 5 0))
